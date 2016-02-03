e01c7f4aa3b70a8548c6435d06057b0f7ef3aa71
Bailey Schroeder <bailey.schroeder@gmail.com>
2016-02-03 15:45:40 -0500

    edits to throw harouku

--- js/app.js
+++ js/app.js
@@ -1,22 +1,33 @@
-$(document) .ready (function(){
-            // when hover Ryu shifts to ready position
-            $(".ryu") .mouseenter(function (){
-                $(".ryu-still") .hide();
-                $(".ryu-ready") .show();
-            })
-            // when stop hover Ryu shifts to still position
-            .mouseleave (function (){
-                $(".ryu-ready") .hide ();
-                $(".ryu-still") .show ();
-            })
-            // click mouse Ryu leans forward and throws Hadouken
-            .mousedown(function(){
-                $('.ryu-ready').hide();
-                $('.ryu-throwing').show();
-            })
-            // release mouse Ryu leans goes back to ready position
-            .mouseup (function(){
-                $(".ryu-throwing") .hide ();
-                $(".ryu-ready") .show ();
-            })
-        })
+$(document).ready(function(){
+    $('.ryu').mouseenter(function(){
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+    })
+    .mouseleave(function() {
+        $('.ryu-still').show();
+        $('.ryu-ready').hide();
+    })
+    .mousedown(function(){
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate(
+            {'left': '1000px'},
+            500,
+            function(){
+                $(this).hide();
+                $(this).css('left', '500px');
+            });
+    })
+    .mouseup(function(){
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    });
+
+});
+
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
